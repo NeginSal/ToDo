5ab5c4c3ba0c0cbca00e4738deed6f803c65eb74
NeginSal <neginsaljooghi@gmail.com>
2021-12-15 12:09:08 +0330

    add Todos component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import Todos from "./Todos.js";
 
-function App() {
+const App = () => {
   const [todos, setTodos] = useState([
     { id: 1, content: "buy some milk" },
     { id: 2, content: "play chess" },
   ]);
-  return <div className="App"></div>;
-}
+  return (
+    <div className="App">
+    <h1 className="center blue-text">Todo's</h1>
+      <Todos todos={todos}/>
+    </div>
+  );
+};
 
 export default App;
