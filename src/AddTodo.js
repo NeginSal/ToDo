1f689b7101729dfb02f2c514d3a82785994824df
NeginSal <neginsaljooghi@gmail.com>
2021-12-30 22:26:18 +0330

    work on addtodo

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,27 +1,27 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const AddTodo = () => {
-     const [content, setContent] = useState('');
-     const handleChange=(e)=>{
-         setContent({
-            content:e.target.value
-         })
-     }
-     const handleSubmit=(e)=>{
-         e.preventDefault();
-         console.log(content)
+const AddTodo = ({addTodo}) => {
+
+  const [content, setContent] = useState("");
+
+  const handleChange = (e) => {
+    setContent( e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(content);
+    setContent('');
 
 
-     }
-    return (
-        <div>
-        <form onSubmit={handleSubmit}>
-            <label> Add new todo:</label>
-            <input type="text" onChange={handleChange} />
-        </form>
-
-        </div>
-     );
-}
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label> Add new todo:</label>
+        <input type="text" value={content} onChange={handleChange} />
+      </form>
+    </div>
+  );
+};
 
 export default AddTodo;
--- src/App.js
+++ src/App.js
@@ -10,11 +10,19 @@ const App = () => {
   const deleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
+  const addTodo =(todo)=>{
+    //todo.id =Math.random();
+    let newtodos = [...todos,todo];
+    setTodos({
+      todos: newtodos
+    })
+
+  }
   return (
     <div className="App container">
       <h1 className="center blue-text">Todo's</h1>
       <Todos todos={todos} deleteTodo={deleteTodo} />
-      <AddTodo/>
+      <AddTodo addTodo={addTodo}/>
     </div>
   );
 };
